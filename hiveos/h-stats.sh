9b85dba019b522807f503478a4b136288566e970
Andrey <dr.andru@gmail.com>
2018-07-09 22:10:57 +0300

    Create h-stats.sh

---
+++ hiveos/h-stats.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#temp=$(jq '.temp' <<< $gpu_stats)
+#fan=$(jq '.fan' <<< $gpu_stats)
+
+stats_raw=`curl --connect-timeout 2 --max-time $API_TIMEOUT --silent --noproxy '*' http://127.0.0.1:60050`
+
+khs=`echo $stats_raw | jq -r '.hashrate.total[0]'`
+stats=`echo $stats_raw | jq '{hs: [.hashrate.threads[][0]], hs_units: "khs", temp: [], fan: [], uptime: .connection.uptime, ar: [.results.shares_good, .results.shares_total - .results.shares_good], algo: "wildkeccak"}'`
+
+#echo khs
+#echo $stats
