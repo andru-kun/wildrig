c37ac849303fe69f3420fe1e8c6922a853577347
Andrey <dr.andru@gmail.com>
2018-07-10 18:08:17 +0300

    Update h-stats.sh

--- hiveos/h-stats.sh
+++ hiveos/h-stats.sh
@@ -3,7 +3,7 @@
 #temp=$(jq '.temp' <<< $gpu_stats)
 #fan=$(jq '.fan' <<< $gpu_stats)
 
-stats_raw=`curl --connect-timeout 2 --max-time $API_TIMEOUT --silent --noproxy '*' http://127.0.0.1:60050`
+stats_raw=`curl --connect-timeout 2 --max-time 5 --silent --noproxy '*' http://127.0.0.1:60050`
 
 khs=`echo $stats_raw | jq -r '.hashrate.total[0]'`
 stats=`echo $stats_raw | jq '{hs: [.hashrate.threads[][0]], hs_units: "khs", temp: [], fan: [], uptime: .connection.uptime, ar: [.results.shares_good, .results.shares_total - .results.shares_good], algo: "wildkeccak"}'`
