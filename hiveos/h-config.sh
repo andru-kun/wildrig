c484bf66531541fccbe887379b455d3bad37c55d
Andrey <dr.andru@gmail.com>
2018-07-10 10:53:35 +0300

    Update h-config.sh

--- hiveos/h-config.sh
+++ hiveos/h-config.sh
@@ -4,7 +4,16 @@
 [[ -z $CUSTOM_TEMPLATE ]] && echo -e "${YELLOW}CUSTOM_TEMPLATE is empty${NOCOLOR}" && return 1
 [[ -z $CUSTOM_URL ]] && echo -e "${YELLOW}CUSTOM_URL is empty${NOCOLOR}" && return 1
 
-[[ -z $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}No CUSTOM_CONFIG_FILENAME is set${NOCOLOR}" && return 1
+CUSTOM_ALGO=wildkeccak
+conf="--url ${CUSTOM_URL} --user ${CUSTOM_TEMPLATE} --pass ${CUSTOM_PASS} --api-port 60050 ${CUSTOM_USER_CONFIG}"
 
-conf="--url ${CUSTOM_URL} --user ${CUSTOM_TEMPLATE} --pass ${CUSTOM_PASS} ${CUSTOM_USER_CONFIG} --api-port 60050"
+#replace tpl values in whole file
+[[ -z $EWAL && -z $ZWAL && -z $DWAL ]] && echo -e "${RED}No WAL address is set${NOCOLOR}"
+[[ ! -z $EWAL ]] && conf=$(sed "s/%EWAL%/$EWAL/g" <<< "$conf")
+[[ ! -z $DWAL ]] && conf=$(sed "s/%DWAL%/$DWAL/g" <<< "$conf")
+[[ ! -z $ZWAL ]] && conf=$(sed "s/%ZWAL%/$ZWAL/g" <<< "$conf")
+[[ ! -z $EMAIL ]] && conf=$(sed "s/%EMAIL%/$EMAIL/g" <<< "$conf")
+[[ ! -z $WORKER_NAME ]] && conf=$(sed "s/%WORKER_NAME%/$WORKER_NAME/g" <<< "$conf") #|| echo "${RED}WORKER_NAME not set${NOCOLOR}"
+
+[[ -z $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}No CUSTOM_CONFIG_FILENAME is set${NOCOLOR}" && return 1
 echo "$conf" > $CUSTOM_CONFIG_FILENAME
